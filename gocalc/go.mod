0a596a80afed7c3d33e5d19df8c276a8f699a5a4
Manu-Abuya <manuabuya69@gmail.com>
2023-06-23 11:30:17 +0300

    main.go commit

---
+++ gocalc/go.mod
@@ -0,0 +1,3 @@
+module github.com/Manu-Abuya/CI-CD-Pipeline-Automation
+
+go 1.18
--- gocalc/main.go
+++ gocalc/main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Welcome to GoCalc")
+	fmt.Println("--------------------------------------")
+
+	for {
+		fmt.Print("Enter an expression (or 'exit' to quit): ")
+		expression, _ := reader.ReadString('\n')
+		expression = strings.TrimSpace(expression)
+
+		if expression == "exit" {
+			break
+		}
+
+		result, err := calculate(expression)
+		if err != nil {
+			fmt.Printf("Error: %s\n", err.Error())
+		} else {
+			fmt.Printf("Results: %.2f\n", result)
+		}
+	}
+}
